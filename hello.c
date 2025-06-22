7608cab788cebd28bb2b434bd7632752fb738cab
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-22 11:12:55 +0600

    datatype

--- hello.c
+++ hello.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int number = 25;
-    char star = '*';
+    int age = 22; // integer data type
     float pi = 3.14;
-
-    int a = 34;
-    a = 40;
-    int A = 29;
-    A = 80;
-
-    int _age = 22;
-    int final_Price = 100;
+    char hastag = '#';
     return 0;
 }
