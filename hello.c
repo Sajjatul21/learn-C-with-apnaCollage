f27d00090b70f9ab5ae68b68c1676b4e7939e32a
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-23 00:34:18 +0600

    chack number divisible by 2 or not

--- hello.c
+++ hello.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int a = 4;
-    int b = 2;
-    a += b; // a = a+b
-    printf("%d \n", a);
-    a -= b; // a = a-b
-    printf("%d \n", a);
-    a *= b; // a = a*b
-    printf("%d \n", a);
-    a %= b; // a = a%b
-    printf("%d \n", a);
+    // write a program to check if a number is divisible by 2 or not;
+
+    int x;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    printf("%d", x % 2 == 0);
     return 0;
 }
