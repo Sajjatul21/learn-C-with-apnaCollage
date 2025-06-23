e3101b241e7652fa36f3a51aa9317d6bbf1f8b13
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-24 01:17:20 +0600

    keep taking numbers as input from user until user enters an odd numbers;

--- hello.c
+++ hello.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 int main()
 {
-    for (int i = 1; i <= 5; i++)
+    // keep taking numbers as input from user until user enters an odd numbers;
+    int n;
+    do
     {
-        if (i == 3)
+        printf("Enter number: ");
+        scanf("%d", &n);
+        printf("%d\n", n);
+
+        if (n % 2 != 0)
         {
             break;
         }
-        printf("%d \n", i);
-    }
-    printf("end");
+    } while (1);
     return 0;
 }
