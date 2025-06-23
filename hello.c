888fc99c353dfff391ee034f29bd3307113bee92
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-24 00:46:23 +0600

    Print the sum of first n natural number. also print them in reverse

--- hello.c
+++ hello.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 int main()
 {
-    int i = 1;
-    do
+    // Print the sum of first n natural number. also print them in reverse;
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    /*
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            sum = sum + i;
+        }
+        printf("Sum is %d\n", sum);
+
+        for (int j = n; j >= 1; j--)
+        {
+            printf("%d \n", j);
+        }
+     */
+
+    int sum = 0;
+    for (int i = 1, j = n; i <= n && j >= 1; i++, j--)
     {
-        printf("%d\n", i);
-        i++;
-    } while (i <= 5);
+        sum = sum + i;
+        printf("%d\n", j);
+    }
+    printf("Sum is %d\n", sum);
 
     return 0;
 }
