aec95077629b449662ece7bd5b1eb4689ee1c347
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-23 14:53:25 +0600

    for loop

--- hello.c
+++ hello.c
@@ -1,32 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int day; // monday=1, tuesday=2, wednesday=3
-    printf("enter your day: ");
-    scanf("%d", &day);
-    switch (day)
+    for (int i = 1; i <= 5; i++)
     {
-    case 1:
-        printf("monday \n");
-        break;
-    case 2:
-        printf("tuesday \n");
-        break;
-    case 3:
-        printf("wednesday \n");
-        break;
-    case 4:
-        printf("thursday \n");
-        break;
-    case 5:
-        printf("friday");
-        break;
-
-    case 6:
-        printf("thursday \n");
-        break;
-        default: printf("not a valid day");
+        printf("%d Hello Shihab \n", i);
     }
-
     return 0;
 }
+
