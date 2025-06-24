ffed0ed0bfdf580b47fab356578f214b34c808e9
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 00:23:39 +0600

    pointer

--- hello.c
+++ hello.c
@@ -1,31 +1,13 @@
 #include <stdio.h>
 
-// write a function to print n terms of the fibonacci sequence.
-
-int fibo(int n);
 
 int main()
 {
-    printf("%d", fibo(6));
+    int age = 22; 
+    int *p = &age;
+    int _age = *p;
+
+    printf("%d",_age);
     return 0;
 }
 
-int fibo(int n)
-{
-    if (n == 0 || n == 1)
-    {
-        if (n == 0)
-        {
-            return 0;
-        }
-        if (n == 1)
-        {
-            return 1;
-        }
-    }
-    int fiboNm1 = fibo(n - 1);
-    int fiboNm2 = fibo(n - 2);
-    int fibN = fiboNm1 + fiboNm2;
-    // printf("fibo of %d is : %d \n", n, fibN);
-    return fibN;
-}
