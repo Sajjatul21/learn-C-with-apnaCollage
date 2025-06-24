56c2ef2485a449cb9613d1c62bb722e3cdc08587
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-24 17:55:15 +0600

    recursive funtion

--- hello.c
+++ hello.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
-#include <math.h>
 
-// write function to calculate area of a square, a circle & a rectangle
-
-float squareArea(float side);
-float circleArea(float rad);
-float rectangleArea(float a, float b);
+void printHw(int count);
 
 int main()
 {
-    float a = 4.0;
-    float b = 2.0;
-    printf("area of square %f\n", squareArea(a));
-    printf("area of circle %f\n", circleArea(b));
-    printf("area of rectangle %f \n", rectangleArea(a, b));
+    printHw(5);
     return 0;
 }
 
-float squareArea(float side)
+// recursive function
+void printHw(int count)
 {
-    return side * side;
-}
-
-float circleArea(float rad)
-{
-    return 3.14 * rad * rad;
-}
-
-float rectangleArea(float a, float b)
-{
-    return a * b;
+    if (count == 0)
+    {
+        return;
+    }
+    printf("Hello World \n");
+    printHw(count - 1);
 }
