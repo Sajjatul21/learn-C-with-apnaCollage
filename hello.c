9dab164d25852d3c410b7465260f51558a150d86
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-24 17:46:12 +0600

    write function to calculate area of a square, a circle & a rectangle

--- hello.c
+++ hello.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 
-// use libary functions to calculate the square of a number given  by user;
+// write function to calculate area of a square, a circle & a rectangle
+
+float squareArea(float side);
+float circleArea(float rad);
+float rectangleArea(float a, float b);
 
 int main()
 {
-    int n = 4;
-    printf("%f", pow(n, 2));
+    float a = 4.0;
+    float b = 2.0;
+    printf("area of square %f\n", squareArea(a));
+    printf("area of circle %f\n", circleArea(b));
+    printf("area of rectangle %f \n", rectangleArea(a, b));
     return 0;
+}
+
+float squareArea(float side)
+{
+    return side * side;
+}
+
+float circleArea(float rad)
+{
+    return 3.14 * rad * rad;
+}
+
+float rectangleArea(float a, float b)
+{
+    return a * b;
 }
