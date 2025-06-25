5cba44f884ec80cf9d246855677200e04d9ff5dd
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 11:53:38 +0600

    swap two value call by reference

--- hello.c
+++ hello.c
@@ -1,22 +1,34 @@
 #include <stdio.h>
 // Swap 2 numbers, a&b
 void swap(int a, int b);
+void _swap(int *a, int *b);
 
 int main()
 {
     int x = 3;
     int y = 5;
     swap(x, y);
-printf("x = %d & x = %d \n", x, y);
+    printf("x = %d & x = %d \n", x, y);
+
+    // pass by reference
+    _swap(&x, &y);
+    printf("x = %d & x = %d \n", x, y);
     return 0;
 }
 
 // call by value
-
 void swap(int a, int b)
 {
     int temp = a;
     a = b;
     b = temp;
     printf("a = %d & b = %d \n", a, b);
+}
+// call by reference
+void _swap(int *a, int *b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+    // printf("a = %d & b = %d \n", a, b);
 }
