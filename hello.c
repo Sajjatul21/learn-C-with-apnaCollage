d8a1a63282229cdd61d5de2399851909ef3a1ef5
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 14:55:51 +0600

    array size

--- hello.c
+++ hello.c
@@ -2,14 +2,12 @@
 // write a program to enter price of 3 item print there final cost with their gst;
 int main()
 {
-    float price[3];
-    printf("enter 3 prices: ");
-    scanf("%f",&price[0]);
-    scanf("%f",&price[1]);
-    scanf("%f",&price[2]);
-    
-    printf("Total price1: %f \n", price[0]+ (price[0]*0.18));
-    printf("Total price2: %f \n", price[0]+ (price[1]*0.18));
-    printf("Total price3: %f \n", price[0]+ (price[2]*0.18));
+
+    // float price[3] = {100.0, 200.0, 300.0};
+    float price[] = {100.0, 200.0, 300.0};
+
+    printf("Total price1: %f \n", price[0] + (price[0] * 0.18));
+    printf("Total price2: %f \n", price[0] + (price[1] * 0.18));
+    printf("Total price3: %f \n", price[0] + (price[2] * 0.18));
     return 0;
 }
