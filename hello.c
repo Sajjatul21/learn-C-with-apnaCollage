5744f5bb694f82b25377174f370450dd7f89b221
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 10:52:59 +0600

    previous pointer to pointer defination wrong. Pointer to pointer: A variable that store  the memory address to anothr variable;

--- hello.c
+++ hello.c
@@ -3,7 +3,9 @@
 int main()
 {
     // pointer to pointer : A variable that store the memory address to another variable;
-    float price = 100.00;
-    float *price = &price;
-    float **price = &price;
-}
+   float price = 100.00;
+    float *pointer = &price;
+    float **pointer = &price; 
+
+    
+} 
