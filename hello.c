0d83d1440d1b716bba3ea4569e8dfd20c81e47e3
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 13:18:50 +0600

    array

--- hello.c
+++ hello.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-// write a function to calculate the sum, product & average of 2 number. Print the average in the main function.
-void doWork(int a, int b, int *sum, int *prod, int *ave);
 
 int main()
 {
-    int a = 4, b = 2;
-    int sum, prod, ave;
-    doWork(a, b, &sum, &prod, &ave);
-    printf("sum = %d \nprod = %d\nave = %d", sum, prod, ave);
-    return 0;
-}
+    int marks[3];
 
-void doWork(int a, int b, int *sum, int *prod, int *ave)
-{
-    /* int sum = a + b;
-    int prod = a * b;
-    int ave = (a + b) / 2; */
-    *sum = a + b;
-    *prod = a * b;
-    *ave = (a + b) / 2;
+    printf("Enter phy: ");
+    scanf("%d", &marks[0]);
+    printf("Enter chem: ");
+    scanf("%d", &marks[1]);
+    printf("Enter math: ");
+    scanf("%d", &marks[2]);
+
+    printf("Phy = %d, chem = %d, math = %d", marks[0], marks[1], marks[3]);
+    return 0;
 }
