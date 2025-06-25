819725ccd21b55df242baaf5ece1a355d3fa1f1e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 09:43:38 +0600

    memory address and value and pointer memory address

--- hello.c
+++ hello.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
-
 int main()
 {
-    int age = 22; 
-    int *p = &age;
-    int _age = *p;
 
-    printf("%d",_age);
+    int age = 22;
+    int * pointer = &age;
+    printf("%u \n", &age);
+    printf("%u \n", pointer);
+    printf("%d \n", *pointer);
+    printf("%u \n",&pointer);
+
+  /*   printf(" normal %d\n", age);
+    printf(" normal %p\n", &age);
+    int *ageMl = &age;
+    printf("age memory location %p \n", ageMl); 
+
+    int *pointer = &age;
+    printf("age memory location %p \n", pointer);
+    printf("age memory location %p \n", *pointer); */
+    // printf("%d", *(&age));
+
+    /*  int age = 22;
+     int *ptr = &age; */
+
+    // value;
+    /*    printf("%d \n", age);
+
+       printf("%d \n", *ptr);
+       printf("%d \n", *(&age)); */
+
     return 0;
 }
-
