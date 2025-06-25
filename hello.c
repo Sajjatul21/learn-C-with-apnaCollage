21d0cfe2fc5c097f910351e3b6aa6da1cd935582
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 17:28:08 +0600

    reverse an array

--- hello.c
+++ hello.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
-// write a function to count the number of odd numbers in an array;
-
-int countOdd(int arr[], int n);
+// write a function to reverse an array
+void reverse(int arr[], int n);
+void printArr(int arr[], int n);
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6};
-    printf("%d", countOdd(arr, 6));
+    reverse(arr, 6);
+    printArr(arr, 6);
     return 0;
 }
 
-int countOdd(int arr[], int n)
+void reverse(int arr[], int n)
 {
-    int count = 0;
+    for (int i = 0; i < n / 2; i++)
+    {
+        int first = arr[i];
+        int second = arr[n - i - 1];
+        arr[i] = second;
+        arr[n - i - 1] = first;
+    }
+}
+
+void printArr(int arr[], int n)
+{
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] % 2 != 0)
-        {
-            count++;
-        }
+        printf("%d \t", arr[i]);
     }
-    return count;
+    printf("\n");
 }
