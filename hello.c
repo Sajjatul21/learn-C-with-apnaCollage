d44bc6e0cb02393e2cf828d8478e4157e0aef54e
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 10:55:21 +0600

    above 2 commit are wrong. pointer To pointer: A variable that store the memory address  to another variable.

--- hello.c
+++ hello.c
@@ -3,9 +3,7 @@
 int main()
 {
     // pointer to pointer : A variable that store the memory address to another variable;
-   float price = 100.00;
+    float price = 100.00;
     float *pointer = &price;
-    float **pointer = &price; 
-
-    
-} 
+    float **pointerToPointer = &pointer;
+}
