e97cb67535108baca526f8b96effc68766ad7053
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 22:20:35 +0600

    string formate specefire

--- hello.c
+++ hello.c
@@ -1,22 +1,10 @@
 #include <stdio.h>
-// create a string firstName  & lastName to store details of user & print all the characters using a loop;
 
-void printString(char arr[]);
 int main()
 {
-    char firstName[] = "Sajjatul";
-    char lastName[] = "Islam";
-    
-    printString(firstName);
-    printString(lastName);
+    char name[50];
+    printf("enter your name: ");
+    scanf("%s", name);
+    printf("your name is %s", name);
     return 0;
-}
-
-void printString(char arr[])
-{
-    for (int i = 0; arr[i] != '\0'; i++)
-    {
-        printf("%c", arr[i]);
-    }
-    printf("\n");
 }
