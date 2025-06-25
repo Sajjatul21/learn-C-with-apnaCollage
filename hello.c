54aff5ca86dae2a1ce3b5c3f7b9d19e106cc64f2
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 11:09:30 +0600

    pointer to pointer

--- hello.c
+++ hello.c
@@ -2,8 +2,16 @@
 
 int main()
 {
-    // pointer to pointer : A variable that store the memory address to another variable;
-    float price = 100.00;
-    float *pointer = &price;
-    float **pointerToPointer = &pointer;
+
+    // print the value of i from its pointer to pointer;
+    int i = 5;
+    int *pointer = &i;
+    printf("%p \n", pointer);  // (&) address : 0061FF18
+    printf("%d \n", *pointer); // (*) value of address: 5
+
+    // pointer to pointer
+    int **pointerToPointer = &pointer;
+    printf("%p \n", pointerToPointer);   // (&) address
+    printf("%p \n", *pointerToPointer);  // (*) value of address
+    printf("%d \n", **pointerToPointer); // (**) value of address pointer to pointer
 }
