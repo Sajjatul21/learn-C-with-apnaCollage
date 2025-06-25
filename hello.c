32bf66e4a4a2d99c3a71b5bd252e3122f5524ad8
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-25 12:01:54 +0600

    // will the address output are same? in call by value

--- hello.c
+++ hello.c
@@ -1,34 +1,16 @@
 #include <stdio.h>
-// Swap 2 numbers, a&b
-void swap(int a, int b);
-void _swap(int *a, int *b);
+// will the address output are same?
+void printAddress(int n);
 
 int main()
 {
-    int x = 3;
-    int y = 5;
-    swap(x, y);
-    printf("x = %d & x = %d \n", x, y);
-
-    // pass by reference
-    _swap(&x, &y);
-    printf("x = %d & x = %d \n", x, y);
+    int n = 4;
+    printAddress(n);
+    printf("Address of n is: %d \n", &n);
     return 0;
 }
 
-// call by value
-void swap(int a, int b)
+void printAddress(int n)
 {
-    int temp = a;
-    a = b;
-    b = temp;
-    printf("a = %d & b = %d \n", a, b);
-}
-// call by reference
-void _swap(int *a, int *b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-    // printf("a = %d & b = %d \n", a, b);
+    printf("Address of n is: %d \n", &n);
 }
