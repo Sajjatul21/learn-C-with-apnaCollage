8b5454ddcb294d899e5504304be6364568d3c0fe
Sajjatul21 <sajjatulislam565@gmail.com>
2025-06-27 12:52:10 +0600

    array of structure

--- hello.c
+++ hello.c
@@ -2,7 +2,6 @@
 #include <string.h>
 
 // write a  program to store the data of 3 student;
-
 struct student
 {
     char name[100];
@@ -12,30 +11,23 @@ struct student
 
 int main()
 {
-    struct student s1;
-    strcpy(s1.name, "Sajjatul");
-    s1.roll = 01;
-    s1.cgpa = 4.00;
+    struct student ece[100];
+    ece[0].roll = 4060;
+    ece[0].cgpa = 4.00;
+    strcpy(ece[0].name, "Sajjatul");
 
-    printf("student name = %s \n", s1.name);
-    printf("student roll = %d \n", s1.roll);
-    printf("student cgpa = %f \n", s1.cgpa);
+    printf("name = %s \n", ece[0].name);
+    printf("roll = %d\n", ece[0].roll);
+    printf("cgpa  = %f\n", ece[0].cgpa);
 
-    struct student s2;
-    strcpy(s2.name, "Shihab");
-    s2.roll = 378482;
-    s2.cgpa = 3.25;
-    printf("student name = %s \n", s2.name);
-    printf("student roll = %d \n", s2.roll);
-    printf("student cgpa = %f \n", s2.cgpa);
+    struct student cse[100];
+    cse[1].roll = 244;
+    cse[1].cgpa = 5.33;
+    strcpy(cse[1].name, "Shihab");
 
-    
-    struct student s3;
-    strcpy(s3.name, "Sourob");
-    s3.roll = 4060;
-    s3.cgpa = 4.3;
-    printf("student name = %s\n", s3.name);
-    printf("student roll = %d \n", s3.roll);
-    printf("student cgpa = %f\n", s3.cgpa);
+    printf("name = %s\n", cse[1].name);
+    printf("roll = %d\n", cse[1].roll);
+    printf("cgpa = %f\n", cse[1].cgpa);
+
     return 0;
 }
